4e3f7a79714f699f0b79f1d0aaee388f3421db5a
Mike Rosengrant <mjrosengrant@yahoo.com>
2015-06-22 10:07:56 -0700

    Began modeling gene to gene clusters
    Updated the cypher queries from the RNeo4j demo to interact with
    current iteration of graph

--- R_scripts/Network_Analysis/rneo4j.R
+++ R_scripts/Network_Analysis/rneo4j.R
@@ -2,22 +2,15 @@ library(RNeo4j)
 
 neo4j = startGraph("http://localhost:7474/db/data")
 
-nodes_query = "
-MATCH (a:Person)-[:ACTED_IN]->(:Movie)
-RETURN DISTINCT ID(a) AS id, a.name AS name
-"
-
-edges_query = "
-MATCH (a1:Person)-[:ACTED_IN]->(:Movie)<-[:ACTED_IN]-(a2:Person)
-RETURN ID(a1) AS source, ID(a2) AS target
-"
+nodes_query = "MATCH (g:Gene) RETURN ID(g) as id, g.symbol AS symbol"
+edges_query="MATCH (g1:Gene)-[r]->(g2:Gene) RETURN DISTINCT g1.symbol AS source,TYPE(r) AS relType,g2.symbol AS target"
 
 nodes = cypher(neo4j, nodes_query)
 edges = cypher(neo4j, edges_query)
 
 library(igraph)
 #create igraph object
-ig = graph.data.frame(edges, directed=FALSE, nodes)
+ig = graph.data.frame(edges, directed=TRUE, nodes)
 
 #Run Girvan-Newman clustering algorithm
 communities = edge.betweenness.community(ig)
@@ -27,14 +20,14 @@ memb = data.frame(name = communities$name, cluster=communities$membership)
 nodes = merge(nodes,memb)
 
 #Reorder columns
-nodes = nodes[c("id", "name", "cluster")]
+nodes = nodes[c("id", "symbol", "cluster")]
 
 #convert nodes and edges to GraphJSON 
 nodes_json = paste0("\"nodes\":", jsonlite::toJSON(nodes))
 edges_json = paste0("\"edges\":", jsonlite::toJSON(edges))
 all_json = paste0("{", nodes_json, ",", edges_json, "}")
 
-sink(file = 'actors.json')
+sink(file = 'pc.json')
 cat(all_json)
 sink()
 
