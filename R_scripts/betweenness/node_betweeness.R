e024d5923112fc00fe50670ff0b3e26ed72432a5
Mike Rosengrant <mjrosengrant@yahoo.com>
2015-07-13 11:49:11 -0700

    Got normalized betweenness values for each node in the graph. The values are in their own csv, as well as added to the pc2_allNodes.csv spreadsheet

--- R_scripts/betweenness/node_betweeness.R
+++ R_scripts/betweenness/node_betweeness.R
@@ -11,7 +11,7 @@ getAllNodesQuery =
 "MATCH n OPTIONAL MATCH (n)-[r]-()
 RETURN DISTINCT 
 
-ID(n) AS id,
+ID(n) AS identifier,
 
 CASE 
 WHEN n:Drug = True
@@ -20,7 +20,7 @@ WHEN n:Gene = True
 THEN n.name
 WHEN n:Aberration = True
 THEN n.type + ' ' + n.value + ' on gene ' + n.gene_entrez  
-End as name,
+End as Title,
 
 CASE
 WHEN n:Gene = True
@@ -31,6 +31,9 @@ LABELS(n) AS label
 ORDER by label[0]
 "
 
+getAllNodesQuery = "MATCH n OPTIONAL MATCH (n)-[r]-()
+RETURN DISTINCT ID(n) AS id"
+
 getAllRelsQuery = "MATCH (n)-[r]->(m) Return ID(n) AS source, ID(m) AS target"
 
 print("Finding Nodes...")
@@ -38,20 +41,26 @@ all_nodes = cypher(neo4j,getAllNodesQuery)
 print("Finding Edges...")
 all_edges = cypher(neo4j, getAllRelsQuery)
 
-full_ig = graph.data.frame(all_edges,directed=TRUE, all_nodes)
+full_ig = graph.data.frame(all_edges,directed=TRUE, vertices = all_nodes)
 print("Calculating Betweenness...")
-all_results = betweenness(ig,directed= TRUE)
-all_results_df <- ldply(all_results,data.frame)
-colnames(all_results_df) <- c("node_name", "betweenness")
+betweenness_results = betweenness(full_ig,directed= TRUE, normalized = TRUE)
+#all_results_df <- ldply(all_results,data.frame)
+#colnames(all_results_df) <- c("node_name", "betweenness")
+
 
 #This file contains betweenness of all nodes
 #To get each class of node's betweenness, load this into the betweeness python script
 #to split into separate CSVs for drugs,biomarkers, etc. 
 write.csv(x= all_nodes ,file="pc2_allnodes_list.csv")
+write.csv(x= all_edges ,file="pc2_alledges_list.csv")
+write.csv(x= betweenness_results ,file="pc2_betweenness_list.csv")
+
+#Merge Betweenness
 
 
-p <- ggplot(all_results_df, aes(betweenness)) + stat_ecdf()
-print(p)
+
+#p <- ggplot(all_results_df, aes(betweenness)) + stat_ecdf()
+#print(p)
 
 
 
--- R_scripts/clustering.R
+++ R_scripts/clustering.R
@@ -1,66 +1,74 @@
 library(RNeo4j)
-#library(NetPathMiner)
+library(igraph)
+
 neo4j = startGraph("http://localhost:7474/db/data")
 
-
 #Gets all Gene and Drug Nodes from DB
-nodes_query = "
-MATCH n WHERE (n:Drug) or (n:Gene)
-RETURN ID(n) AS id,
+nodes_query = 
+"MATCH n OPTIONAL MATCH (n)-[r]-()
+RETURN DISTINCT 
+
+ID(n) AS id,
+
 CASE 
-WHEN n:Gene = True
-THEN n.symbol
 WHEN n:Drug = True
 THEN n.name
+WHEN n:Gene = True
+THEN n.name
+WHEN n:Aberration = True
+THEN n.type + ' ' + n.value + ' on gene ' + n.gene_entrez  
 End as name,
-labels(n) AS label
-Order by label[0]
+
+CASE
+WHEN n:Gene = True
+THEN n.entity_class 
+END AS entity_class,
+
+LABELS(n) AS label 
+ORDER by label[0]
 "
 #Returns list of edges between drugs and genes 
-edges_query="MATCH (n:Drug)-[r:TARGETS_GENE]->(m:Gene) RETURN ID(n) AS source, ID(m) AS target"
+edges_query="MATCH (n)-->(m) RETURN ID(n) AS source, ID(m) AS target"
 
-#Returns number of connections each drug node has
-drug_gene_connection_count = "MATCH (n:Drug)-[r]->(g:Gene) 
-return n.name AS drug, count(r) AS connection_count ORDER BY -connection_count"
+drugs_query = "MATCH (d:Drugs) return ID(d) AS id, d.name AS name"
+drug_edges_query = "MATCH (d:Drugs)-->(n) return ID(d) as source, ID(n) as target
+UNION MATCH (n)-->(d:Drugs) return ID(n) as source, ID(d) as target"
 
 print("Finding Nodes...")
 nodes = cypher(neo4j, nodes_query)
 print("Finding Edges...")
 edges = cypher(neo4j, edges_query)
 
+nodes_df = do.call(rbind.data.frame, nodes)
+edges_df = do.call(rbind.data.frame, edges)
 
-library(igraph)
 #create igraph object
-ig = graph.data.frame(edges, directed=TRUE, nodes)
+ig = graph.data.frame(edges_df, directed=TRUE, nodes_df)
+
 
 #http://www.inside-r.org/packages/cran/igraph/docs/transitivity
-cc = transitivity(ig, vids=NULL, type="local")
+#print("Calculating Clustering Coefficients...")
+#cc = transitivity(ig, vids=NULL, type="local")
 
-print("Calculating Clustering Coefficients...")
-print(cc)
+#table = merge(nodes,cc)
+
+#print("Writing clustering coefficients to CSV...")
+#write.table(table, file = "/node_info/allnodes_clustering.csv", append = FALSE, quote = TRUE, sep = ",",
+ #           eol = ";", na = "NA", dec = ".",
+  #          col.names = TRUE,
+   #         fileEncoding = "utf-8")
+
+
 #Run Girvan-Newman clustering algorithm
-print("Running Girvan Newman betweenness algorithm")
-communities = edge.betweenness.community(ig)
+#print("Running Girvan Newman betweenness algorithm")
+#communities = edge.betweenness.community(ig)
 
-print ("Done calculating communities")
+#print ("Done calculating communities")
 #Extract cluster assignments and merge with nodes data.frame
-memb = data.frame(name = communities$name, cluster=communities$membership)
-nodes = merge(nodes,memb)
+#memb = data.frame(name = communities$name, cluster=communities$membership)
+#nodes = merge(nodes,memb)
 
 #Reorder columns
-nodes = nodes[c("id", "name", "label", "cluster")]
-
-#print("New Nodes table with clustering:")
-#print(nodes)
-
-#convert nodes and edges to GraphJSON 
-nodes_json = paste0("\"nodes\":", jsonlite::toJSON(nodes))
-edges_json = paste0("\"edges\":", jsonlite::toJSON(edges))
-all_json = paste0("{", nodes_json, ",", edges_json, "}")
-
-sink(file = 'pc.json')
-cat(all_json)
-sink()
+#nodes = nodes[c("id", "name", "label", "cluster")]
 
 
-modifiers = cypher.execute(neo4j, "MATCH (d:Drug)-[r:TARGETS_GENE]->(g:Gene {entity_class:"modifier"}) return d.name AS drug_name, g.symbol AS gene_name")
