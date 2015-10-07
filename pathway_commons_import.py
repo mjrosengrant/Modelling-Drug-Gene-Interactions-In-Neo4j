5c2758aa81505e021faa9c617ebb7ce62c29029a
Mike <mjrosengrant@yahoo.com>
2015-10-07 07:49:15 -0700

    Updated pathway_commons_import.py to import nodes and relationships into Mongo

--- pathway_commons_import.py
+++ pathway_commons_import.py
@@ -31,25 +31,65 @@ def createAllGenes(graph,data):
     #Create Dictionary that takes gene symbol as input and return entrez value
     #entrezDict = createEntrezDict()
 
-    #uniqueGenes = np.unique(data[['PARTICIPANT_A','PARTICIPANT_B']])        
-    uniqueGenesA = np.unique(data["PARTICIPANT_A"])
-    uniqueGenesB = np.unique(data["PARTICIPANT_B"])
-
-    #print str(uniqueGenes.size) + "Unique Genes Found"
-    print "uniqueGenesA.size = " + str(uniqueGenesA.size)
-    print "uniqueGenesB.size = " + str(uniqueGenesB.size)
-    #print "uniqueGene.size = " + str(uniqueGenes.size)
-
-
-    #graph.nodes.ensureIndex({"name":1},{"unique":"true"});
+    uniqueGenes = np.unique( data['PARTICIPANT_A'].append( data['PARTICIPANT_B'] ) )
     
-    for i in range(uniqueGenesA.size):
-        print uniqueGenesA[i] + "\n"
-        graph.nodes.insert_one({"name": str(uniqueGenesA[i])} )
+    for i in range(uniqueGenes.size):
+        print uniqueGenes[i] + "\n"
+        graph.nodes.insert_one({"name": str(uniqueGenes[i])} )        
+
+def createEdges(graph,data):
+    
+    for i in range(data["INTERACTION_TYPE"].size):
+        print str(data["PARTICIPANT_A"][i]) + "\t" + str(data["INTERACTION_TYPE"][i]) + "\t" + str(data["PARTICIPANT_B"][i])
         
+        part_a = graph.nodes.find_one({"name": data["PARTICIPANT_A"][i]})
+        part_b = graph.nodes.find_one({"name": data["PARTICIPANT_B"][i]})
 
-def createRelationships(graph,data):
-    
+        graph.edges.insert_one(
+            {
+            #"INTERACTION_TYPE": data["INTERACTION_TYPE"][i],
+            #"PARTICIPANT_B": data["PARTICIPANT_B"][i]
+            #"PARTICIPANT_A": data["PARTICIPANT_A"][i], 
+
+            "PARTICIPANT_A": part_a["_id"], 
+            "INTERACTION_TYPE": data["INTERACTION_TYPE"][i],
+            "PARTICIPANT_B": part_b["_id"]
+            } 
+        )
+
+def main():
+    print "Starting Main Function"
+    data = pd.read_csv('data/pc_data_extended.csv')
+
+    client = MongoClient()
+    graph = client.hypergraph
+
+    createAllGenes(graph,data)
+    createEdges(graph,data)
+
+
+def neo4j_authenticate():
+    authenticate("localhost:7474", "neo4j", "qwerty1")
+    graph = Graph()
+
+def neo4j_createAllGenes(uniqueGenes,entrezDict):
+    for i in range(uniqueGenes.size):
+        
+        geneName = str(uniqueGenes[i])
+        entrez = ""
+        
+        if geneName in entrezDict:
+            entrez = str(entrezDict[geneName])
+            
+        query = "MERGE (g:Gene {{name:\"{0}\", entrez:\"{1}\"}} )".format( geneName, entrez)
+        
+        currentIsGene = isGene(geneName)
+        if currentIsGene is True:
+            print query +"\n"            
+            graph.cypher.execute(query)
+        else:
+            print geneName + " is not a gene. Skipping"               
+def neo4j_createAllRelationships(graph,data):
     for i in range(data["INTERACTION_TYPE"].size):
         
         #Makes sure that the Interaction Type does not involve proteins or Molecules
@@ -76,38 +116,5 @@ def createRelationships(graph,data):
             print "At least one of the participants is not a gene. Skipping row " + str(i)
             continue
 
-def main():
-    print "Starting Main Function"
-    data = pd.read_csv('data/pc_data_extended.csv')
-    #data = pd.read_csv('data/PathwayCommons.7.Reactome.EXTENDED_BINARY_SIF.csv',sep=",")
-
-    print data.shape
-    client = MongoClient()
-    graph = client.hypergraph
-
-    createAllGenes(graph,data)
-    #createRelationships(graph,data)
-
-
-def neo4j_authenticate():
-    authenticate("localhost:7474", "neo4j", "qwerty1")
-    graph = Graph()
-def neo4j_createAllGenes(uniqueGenes,entrezDict):
-    for i in range(uniqueGenes.size):
-        
-        geneName = str(uniqueGenes[i])
-        entrez = ""
-        
-        if geneName in entrezDict:
-            entrez = str(entrezDict[geneName])
-            
-        query = "MERGE (g:Gene {{name:\"{0}\", entrez:\"{1}\"}} )".format( geneName, entrez)
-        
-        currentIsGene = isGene(geneName)
-        if currentIsGene is True:
-            print query +"\n"            
-            graph.cypher.execute(query)
-        else:
-            print geneName + " is not a gene. Skipping"               
 
 main()
